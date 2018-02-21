93fa6201d7c1439fb65e76e9fbc594994ce7095c
echo <echo@echo.local>
2018-02-21 20:05:00 +0000

    PL01

---
+++ pl01/src/main/java/com/diogocapela/pl01/Main.java
@@ -0,0 +1,18 @@
+package com.diogocapela.pl01;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Qual o nome da pessoa?");
+        String nome = scanner.nextLine();
+        
+        
+        
+    }
+    
+}
